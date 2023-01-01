bcd822ef4aff3f20481c661a1fca4a965f31d558
Mcdxmmd <104222278+Mcdxmmd@users.noreply.github.com>
2023-01-01 12:54:31 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,37 @@
+<script>
+    // Delete if you want
+    console.log("Hello")
+    // Now don't delete
+$('a[href*="#"]')
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) {
+            return false;
+          } else {
+            $target.attr('tabindex','-1');
+            $target.focus();
+          };
+        });
+      }
+    }
+  });
+
+  // robot rd once
+
+</script>
